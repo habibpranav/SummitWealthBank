9e826b0ad6249871853618b53841b6a1645317a8
pranavhabib <pranavhabib1@gmail.com>
2025-11-15 17:53:43 -0500

    Added Frontend modules

---
+++ summit-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AccountForm from './components/AccountForm';
+
+function App() {
+    return (
+        <div>
+            <h1>Summit Wealth Bank</h1>
+            <AccountForm />
+        </div>
+    );
+}
+
+export default App;
