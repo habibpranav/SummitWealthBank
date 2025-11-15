3339cc3cb703ce0c7de5f4aa8319d0e8d1f0fd06
pranavhabib <pranavhabib1@gmail.com>
2025-11-15 18:46:11 -0500

    Default fallback file

--- src/main/java/edu/ssw590/summitwealthbank/dto/AccountOpenRequest.java
+++ src/main/java/edu/ssw590/summitwealthbank/dto/AccountOpenRequest.java
@@ -3,8 +3,11 @@ package edu.ssw590.summitwealthbank.dto;
 import edu.ssw590.summitwealthbank.model.Account;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class AccountOpenRequest {
     private Long userId;
     private Account.AccountType type;
+    private BigDecimal initialDeposit;
 }
