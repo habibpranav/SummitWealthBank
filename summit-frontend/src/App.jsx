6d3db25209499a4f51f051ca5129ad725c867fb8
pranavhabib <pranavhabib1@gmail.com>
2025-11-15 23:21:56 -0500

    All the valid Frontend changes for login and register

--- summit-frontend/src/App.jsx
+++ summit-frontend/src/App.jsx
@@ -1,34 +1,75 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 import OpenAccount from "./components/OpenAccount";
 import ViewAccounts from "./components/ViewAccounts";
-import Transfer from "./components/Transfer";
+import TransferMoney from "./components/TransferMoney";
 import Admin from "./components/Admin";
 import CardFeed from "./components/CardFeed";
 import Wealth from "./components/Wealth";
+import PrivateRoute from "./auth/PrivateRoute";
+import Navbar from "./components/Navbar";
 
 export default function App() {
     return (
         <Router>
             <div className="min-h-screen bg-gray-100 p-6">
-                <nav className="mb-6 space-x-4">
-                    <Link to="/" className="text-blue-600">Home</Link>
-                    <Link to="/open-account" className="text-blue-600">Open Account</Link>
-                    <Link to="/view-accounts" className="text-blue-600">View Accounts</Link>
-                    <Link to="/transfer" className="text-blue-600">Transfer</Link>
-                    <Link to="/admin" className="text-blue-600">Admin</Link>
-                    <Link to="/card-feed" className="text-blue-600">Card Feed</Link>
-                    <Link to="/wealth" className="text-blue-600">Wealth</Link>
-                </nav>
+                <Navbar /> {/* Use Navbar instead of hardcoded <nav> */}
 
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/open-account" element={<OpenAccount />} />
-                    <Route path="/view-accounts" element={<ViewAccounts />} />
-                    <Route path="/transfer" element={<Transfer />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/card-feed" element={<CardFeed />} />
-                    <Route path="/wealth" element={<Wealth />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    <Route
+                        path="/open-account"
+                        element={
+                            <PrivateRoute>
+                                <OpenAccount />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/view-accounts"
+                        element={
+                            <PrivateRoute>
+                                <ViewAccounts />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/transfer"
+                        element={
+                            <PrivateRoute>
+                                <TransferMoney />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin"
+                        element={
+                            <PrivateRoute>
+                                <Admin />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/card-feed"
+                        element={
+                            <PrivateRoute>
+                                <CardFeed />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/wealth"
+                        element={
+                            <PrivateRoute>
+                                <Wealth />
+                            </PrivateRoute>
+                        }
+                    />
                 </Routes>
             </div>
         </Router>
